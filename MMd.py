14aea0b6925f950edf8e6ece557a7f21c84b3114
kiankenevisi <k33129650@gmail.com>
2025-10-22 19:35:55 +0330

    Add files via upload

---
+++ MMd.py
@@ -0,0 +1,11 @@
+import turtle
+t=turtle.Pen()
+def mysquare(size):
+    for x in range(1,5):
+        t.forward(size)
+        t.lt(90)
+for i in range(0,1000000000000000000000):
+    mysquare(50)        
+
+    
+    
