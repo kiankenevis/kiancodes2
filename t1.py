11cae8cb42d03bd53afe58a578dd28a370701aba
kiankenevisi <k33129650@gmail.com>
2025-10-29 20:12:47 +0330

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,8 @@
+num = int(input('adad1 ='))
+num2=int(input('adad2 ='))
+num3=int(input('adad3 ='))
+def kian(num,num2,num3):
+    print(num,'+', num2,'+',num3,'=',num+num2+num3)
+    print(num,'-', num2,'-',num3,'=',num-num2-num3)
+    print(num,'*', num2,'*',num3,'=',num*num2*num3)
+kian(num,num2,num3)
